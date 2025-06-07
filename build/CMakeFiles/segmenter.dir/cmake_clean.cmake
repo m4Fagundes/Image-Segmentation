c4e7071dd72bdd097c32523c56df541f65d809da
Matheus Fagundes <124789617+m4Fagundes@users.noreply.github.com>
2025-06-07 17:06:59 -0300

    feat(project): inicializa estrutura base do projeto de segmentação
    Implementa a fundação inicial do projeto, incluindo:
    - Estruturação de diretórios (/src, /include, /build, etc.).
    - Configuração do sistema de build com CMake (CMakeLists.txt).
    - Adição da classe 'Imagem' para manipulação de arquivos de imagem, utilizando a biblioteca stb_image.
    - Adição da classe 'Grafo' como estrutura de dados base para representação da imagem.
    - Criação de um ponto de entrada (main.cpp) para testes iniciais.

---
+++ build/CMakeFiles/segmenter.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/segmenter.dir/src/Grafo.cpp.obj"
+  "CMakeFiles/segmenter.dir/src/Grafo.cpp.obj.d"
+  "CMakeFiles/segmenter.dir/src/Imagem.cpp.obj"
+  "CMakeFiles/segmenter.dir/src/Imagem.cpp.obj.d"
+  "CMakeFiles/segmenter.dir/src/main.cpp.obj"
+  "CMakeFiles/segmenter.dir/src/main.cpp.obj.d"
+  "libsegmenter.dll.a"
+  "segmenter.exe"
+  "segmenter.exe.manifest"
+  "segmenter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/segmenter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/segmenter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for segmenter.
+# This may be replaced when dependencies are built.
